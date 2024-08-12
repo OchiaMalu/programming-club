134a57adf9b7f137a3f68e9293fa92e72983fb2e
ochiamalu <linzhehao1108@126.com>
2024-08-12 16:34:44 +0800

    feat: 鉴权模块骨架建立

---
+++ pro-club-auth/pro-club-auth-starter/src/main/java/com/ochiamalu/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package com.ochiamalu.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 鉴权模块启动类
+ *
+ * @author OchiaMalu
+ * @date 2024/08/12
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
