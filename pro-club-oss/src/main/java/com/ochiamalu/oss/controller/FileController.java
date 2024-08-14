7b9a0053765ac61c4d2d08302c35a006d132c78e
ochiamalu <linzhehao1108@126.com>
2024-08-14 18:26:05 +0800

    feat: 新增上传文件接口

--- pro-club-oss/src/main/java/com/ochiamalu/oss/controller/FileController.java
+++ pro-club-oss/src/main/java/com/ochiamalu/oss/controller/FileController.java
@@ -4,8 +4,10 @@ import com.ochiamalu.oss.service.FileService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 文件控制器
@@ -20,8 +22,18 @@ public class FileController {
     @Resource
     private FileService fileService;
 
-    @GetMapping("/getAllName")
-    public String getAllName(){
-        return fileService.getUrl("","");
+    @GetMapping("/upload")
+    public String uploadFile(MultipartFile uploadFile, String bucket, String objectName) {
+        return fileService.uploadFile(uploadFile, bucket, objectName);
+    }
+
+    @GetMapping("/getUrl")
+    public String getUrl(String bucket, String objectName) {
+        return fileService.getUrl(bucket, objectName);
+    }
+
+    @GetMapping("/getAllBucket")
+    public List<String> getAllBucket() {
+        return fileService.getAllBucket();
     }
 }
